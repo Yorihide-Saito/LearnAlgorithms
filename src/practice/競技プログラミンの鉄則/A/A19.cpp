b9954c3e3d1d6fa10cb6e55d00176cabd7b2a506
Yorihide-Saito <yorihide.saito@gmail.com>
2025-09-03 02:13:31 +0900

    動的計画法のナプサック問題

---
+++ src/practice/競技プログラミンの鉄則/A/A19.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+long long N, W, w[109], v[109];
+long long dp[109][100009];
+
+int main () {
+    cin >> N >> W;
+    for (int i = 1; i <= N; i++) cin >> w[i] >> v[i];
+
+    for (int i = 0; i <= N; i++) {
+        for (int j = 0; j <= W; j++) {
+            dp[i][j] = -1,000,000,000,000,000LL;
+        }
+    }
+
+    dp[0][0] = 0;
+    for (int i = 1; i <= N; i++) {
+        for (int j = 0; j <= W; j++) {
+            if(w[i] > j) {
+                dp[i][j] = dp[i - 1][j];
+            } else {
+                dp[i][j] = max(dp[i-1][j], dp[i-1][j-w[i]] + v[i]);
+            }
+        }
+    }
+    long long Answer = 0;
+    for (int i = 0; i <= W; i++) Answer = max(Answer, dp[N][i]);
+    cout << Answer << endl;
+
+    return 0;
+}
