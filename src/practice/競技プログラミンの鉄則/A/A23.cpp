3bc9830df2d0a9e51b79d0783a2d1df35b05025d
Yorihide-Saito <yorihide.saito@gmail.com>
2025-09-06 19:33:41 +0900

    ビットDP追加

---
+++ src/practice/競技プログラミンの鉄則/A/A23.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+int N, M;
+int a[109][19], dp[109][1024];
+
+
+int main() {
+    cin >> N >> M;
+    for (int i = 1; i <= M; i++) {
+        for (int j = 1; j <= N; j++) {
+            cin >> a[i][j];
+        }
+    }
+
+    for (int i = 0; i <= M; i++) {
+        for (int j = 0; j < (1 << N); j++) {
+            dp[i][j] = 1'000'000'000;
+        }
+    }
+
+    dp[0][0] = 0;
+    for (int i = 1; i <= M; i++) {
+        for (int j = 0; j < (1 << N); j++) {
+            int already[19];
+            for (int k = 1; k <= N; k++) {
+                if ((j / (1 << (k-1))) % 2 == 0) already[k] = 0;
+                else already[k] = 1;
+            }
+
+            int v = 0;
+            for (int k = 1; k <= N; k++) {
+                if (already[k] == 1 || a[i][k] == 1) v += (1 << (k-1));
+            }
+
+            dp[i][j] = min(dp[i][j], dp[i-1][j]);
+            dp[i][v] = min(dp[i][v], dp[i-1][j] + 1);
+        }
+    }
+
+    if (dp[M][(1 << N) - 1] == 1'000'000'000) cout << "-1" << endl;
+    else cout << dp[M][(1 << N) - 1] << endl;
+    return 0;
+}
