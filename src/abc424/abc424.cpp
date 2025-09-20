8ba29b24530f1404918dcaf995752807ec2d7432
Yorihide-Saito <yorihide.saito@gmail.com>
2025-09-21 02:29:14 +0900

    Cの正解コード記載（隣接グラフなのでN+1*N+1の２次元配列じゃなくて良かったらしい。）

--- src/abc424/abc424.cpp
+++ src/abc424/abc424.cpp
@@ -1,58 +1,51 @@
 #include<iostream>
 #include<vector>
 #include<queue>
-#include<algorithm>
 using namespace std;
 
-vector<vector<int>> buildGraph(const vector<pair<int, int>>& skill, int N) {
-    vector<vector<int>> graph(N+1, vector<int>(N+1, 0));
+vector<vector<int>> buildGraph(const vector<int> &a, const vector<int> &b, int N) {
+    vector<vector<int>> graph(N+1);
     for (int i = 1; i <= N; i++) {
-        int a = skill[i].first, b = skill[i].second;
-        if (1 <= a) graph[a][i] = 1;
-        if (1 <= b) graph[b][i] = 1;
+        if (a[i] == 0 && b[i] == 0) {
+            continue;
+        }
+        graph[a[i]].push_back(i);
+        graph[b[i]].push_back(i);
     }
     return graph;
 }
 
-void bfs(vector<int>& start, const vector<vector<int>>& graph, vector<int>& haveSkill) {
-    queue<int> q;
-    for (int s : start) {
-        if (!haveSkill[s]) { haveSkill[s] = 1; q.push(s); }
+int main() {
+    int N; cin >> N;
+    vector<int> A(N+1, 0), B(N+1, 0);
+    for (int i = 1; i <= N; i++) {
+        cin >> A[i] >> B[i];
     }
-    int N = (int)haveSkill.size() - 1;
+
+    vector<vector<int>> graph = buildGraph(A, B, N);
+    // BFS
+    vector<int> visited(N + 1, 0);
+    queue<int> q;
+    for (int i = 1; i <= N; i++) {
+        if (A[i] == 0 && B[i] == 0) {
+            visited[i] = 1;
+            q.push(i);
+        }
+    }
 
     while (!q.empty()) {
-        int v = q.front(); q.pop();
-        for (int u = 1; u <= N; ++u) {
-            if (graph[v][u] && !haveSkill[u]) {
-                haveSkill[u] = 1;
-                q.push(u);
+        int v = q.front();
+        q.pop();
+        for (int u = 0; u < graph[v].size(); u++) {
+            if (!visited[graph[v][u]]) {
+                visited[graph[v][u]] = 1;
+                q.push(graph[v][u]);
             }
         }
     }
-}
-
-int main() {
-    int N; cin >> N;
-    vector<pair<int, int>> skill(N+1, { 0, 0 });
-    for (int i = 0; i < N; i++) cin >> skill[i].first >> skill[i].second;
-
-    vector<vector<int>> graph = buildGraph(skill, N);
-    vector<int> haveSkill(N + 1, 0);
-
-    vector<int> startPoint;
-    for (int i = 1; i <= N; i++) {
-        if (skill[i].first == 0 && skill[i].second == 0) {
-            startPoint.push_back(i);
-        }
-    }
-
-    bfs(startPoint, graph, haveSkill);
 
     int count = 0;
-    for (int i = 1; i <= N; i++) {
-        if (haveSkill[i]) count += 1;
-    }
-
+    for (int i = 1; i <= N; i++) if (visited[i]) count++;
     cout << count << endl;
+    return 0;
 }
