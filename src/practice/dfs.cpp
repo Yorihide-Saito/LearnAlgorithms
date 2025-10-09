4a387a40dcc31c3a823305b1477a49dc39177946
Yorihide-Saito <yorihide.saito@gmail.com>
2025-10-10 04:10:51 +0900

    貪欲法によす最小構成を作成するアルゴリズムを作成

---
+++ src/practice/dfs.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<string>
+#include<vector>
+#include<stack>
+using namespace std;
+
+int main() {
+    int h, w; cin >> h >> w;
+    vector<string> map;
+    for (int i = 0; i < h; i++) {
+        cin >> map[i];
+    }
+
+    return 0;
+}
