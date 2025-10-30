4358ba345c39642471dd1073019623b1db489ada
Yorihide-Saito <yorihide.saito@gmail.com>
2025-10-31 03:43:12 +0900

    ダイクストラを使用した経路の最短問題の練習

--- src/typical90/typical90.cpp
+++ src/typical90/typical90.cpp
@@ -1,74 +1,39 @@
 #include<iostream>
 #include<vector>
-#include<numeric>
+#include<utility>
+#include<queue>
+#include<algorithm>
 using namespace std;
 
-struct DSU {
-    int n;
-    vector<int> parent, sz;
-    DSU(int n): n(n), parent(n), sz(n,1) {
-        iota(parent.begin(), parent.end(), 0);
-    }
-    int find(int x) {
-        if (parent[x] == x) return x;
-        // 経路圧縮
-        return parent[x] = find(parent[x]);
-    }
-
-    bool unite(int a, int b) {
-        a = find(a); b = find(b);
-        if (a == b) return false;
-        if (sz[a] < sz[b]) swap(a, b);
-        parent[b] = a;
-        sz[a] += sz[b];
-        return true;
-    }
-
-    bool same(int a, int b) { return find(a) == find(b); }
-};
+const int INF = (int) 1 << 30;
 
 int main() {
-    int h, w; cin >> h >> w;
+    int n, m; cin >> n >> m;
+    vector<vector<pair<int, int>>> graph(n);
+    for (int i = 0; i < m; i++) {
+        int a, b, c; cin >> a >> b >> c;
+        a--; b--;
+        graph[a].emplace_back(b, c);
+        graph[b].emplace_back(a, c);
+    }
 
-    const long long n = 1LL * h * w;
-    DSU dsu((int)n);
-    vector<unsigned char> red(n, 0);
+    vector<int> dist(n, INF);
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int ,int>>> pq;
+    dist[0] = 0;
+    pq.emplace(0, 0);
 
-    auto id = [&](int r, int c) ->int {
-        return (r-1) * w + (c-1);
-    };
-
-    const int dr[4] = {-1, 1, 0, 0};
-    const int dc[4] = {0, 0, -1, 1};
-
-    int q; cin >> q;
-
-    while(q--) {
-        int command;
-        cin >> command;
-        if (command == 1) {
-            int r, c; cin >> r >> c;
-            int v = id(r, c);
-            if (!red[v]) {
-                red[v] = 1;
-                for (int k = 0; k < 4; k++) {
-                    int nr = r + dr[k], nc = c + dc[k];
-                    if (nr < 1 || nr > h || nc < 1|| nc > w) continue;
-                    int u = id(nr, nc);
-                    if (red[u]) dsu.unite(v, u);
-                }
-            }
-        }
-        else {
-            int ra, ca, rb, cb;
-            cin >> ra >> ca >> rb >> cb;
-            int a = id(ra, ca), b = id(rb, cb);
-            if(red[a] && red[b] && dsu.same(a, b)) {
-                cout << "Yes" << endl;
-            } else {
-                cout << "No" << endl;
+    while(!pq.empty()) {
+        auto [d, v] = pq.top(); pq.pop();
+        if (d > dist[v]) continue;
+        for (auto& [to, w] : graph[v]) {
+            int nd = d + w;
+            if (nd < dist[to]) {
+                dist[to] = nd;
+                pq.emplace(nd, to);
             }
         }
     }
+
+    cout << dist[n - 1] << endl;
     return 0;
 }
--- src/typical90/typical90_013.cpp
+++ src/typical90/typical90_013.cpp
@@ -1,8 +1,47 @@
 #include<iostream>
+#include<vector>
+#include<utility>
+#include<queue>
+#include<algorithm>
 using namespace std;
+
+const int INF = (int) 1 << 30;
+
+vector<int> dijkstra(vector<vector<pair<int, int>>> &graph, const int n, const int start) {
+    vector<int> dist(n, INF);
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int ,int>>> pq;
+    dist[start] = 0;
+    pq.emplace(0, start);
+
+    while(!pq.empty()) {
+        auto [d, v] = pq.top(); pq.pop();
+        if (d > dist[v]) continue;
+        for (auto& [to, w] : graph[v]) {
+            int nd = d + w;
+            if (nd < dist[to]) {
+                dist[to] = nd;
+                pq.emplace(nd, to);
+            }
+        }
+    }
+    return dist;
+}
 
 int main() {
     int n, m; cin >> n >> m;
-    
+    vector<vector<pair<int, int>>> graph(n);
+    for (int i = 0; i < m; i++) {
+        int a, b, c; cin >> a >> b >> c;
+        a--; b--;
+        graph[a].emplace_back(b, c);
+        graph[b].emplace_back(a, c);
+    }
+
+    vector<int>dist_0 = dijkstra(graph, n, 0);
+    vector<int>dist_n = dijkstra(graph, n, n - 1);
+
+    for (int i = 0; i < n; i++) {
+        cout << dist_0[i] + dist_n[i] << endl;
+    }
     return 0;
 }
