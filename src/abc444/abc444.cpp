6bc2647175e05ae07ce27668c7abda20cbd12446
Yorihide-Saito <yorihide.saito@gmail.com>
2026-02-08 22:34:25 +0900

    D問題実施、いもす使用する対応で完了

--- src/abc444/abc444.cpp
+++ src/abc444/abc444.cpp
@@ -7,28 +7,34 @@ using namespace std;
 using ll = long long;
 
 int main() {
-    int N; cin >> N;
+    int N;
+    cin >> N;
     vector<int> A(N);
     for (auto &a : A) cin >> a;
 
     sort(A.begin(), A.end());
-    int size_A = A.size();
-
-    map<ll, ll> m;
-    for (int i = 0; i < size_A; i++) {
-        m[A[i]] += 1;
+    int maxA = A.back();
+    vector<int> diff(maxA + 1);
+    for (int i = 0; i < A.size(); i++) {
+        diff[0] += 1;
+        diff[A[i]] -= 1;
     }
 
-    int current_k = 0;
-    int sum_v = 0;
-    for (auto [k, v] : m) {
-        sum_v += v;
-        for (int i = current_k; i < k; i++) {
-            cout << sum_v;
-        }
-        current_k = k;
+    vector<int> c(maxA + 1);
+    int current = 0;
+    for (int i = 0; i < maxA; i++) {
+        current += diff[i];
+        c[i] = current;
     }
 
+    for (int i = 0; i < c.size() - 1; i++) {
+        c[i+1] += c[i] / 10;
+        c[i] = c[i] % 10;
+    }
+
+    int i = c.size() - 1;
+    while(i > 0 && c[i] == 0) i--;
+    for(; i >= 0; i--) cout << c[i];
     cout << endl;
     return 0;
 }
--- src/abc444/abc444_d.cpp
+++ src/abc444/abc444_d.cpp
@@ -7,25 +7,34 @@ using namespace std;
 using ll = long long;
 
 int main() {
-    int N; cin >> N;
+    int N;
+    cin >> N;
     vector<int> A(N);
     for (auto &a : A) cin >> a;
 
     sort(A.begin(), A.end());
-    int size_A = A.size();
-
-    map<ll, ll> m;
-    for (int i = 0; i < size_A; i++) {
-        m[A[i]] += 1;
+    int maxA = A.back();
+    vector<int> diff(maxA + 1);
+    for (int i = 0; i < A.size(); i++) {
+        diff[0] += 1;
+        diff[A[i]] -= 1;
     }
 
-    ll ans = 0;
-    for (auto [k, v] : m) {
-        ll y = 0;
-        for (ll i = 0; i < k; i++) y = y * 10 + v;
-        ans += y;
+    vector<int> c(maxA + 1);
+    int current = 0;
+    for (int i = 0; i < maxA; i++) {
+        current += diff[i];
+        c[i] = current;
     }
 
-    cout << ans << endl;
+    for (int i = 0; i < c.size() - 1; i++) {
+        c[i+1] += c[i] / 10;
+        c[i] = c[i] % 10;
+    }
+
+    int i = c.size() - 1;
+    while(i > 0 && c[i] == 0) i--;
+    for(; i >= 0; i--) cout << c[i];
+    cout << endl;
     return 0;
 }
