d7dd8ed507711f823d6978eb13ead17ecc19064a
Yorihide-Saito <yorihide.saito@gmail.com>
2026-05-26 00:19:42 +0900

    459 E 問題

--- src/abc458/abc458.cpp
+++ src/abc458/abc458.cpp
@@ -6,38 +6,51 @@
 #include<functional>
 using namespace std;
 using ll = long long;
+const long long MOD = 998244353;
+
+long long fact[3000011], invFact[3000011];
+
+long long modpow(long long a, long long e) {
+    long long r = 1;
+    while(e) {
+        if (e & 1) r = r * a % MOD;
+        a = a * a % MOD;
+        e >>= 1;
+    }
+    return r;
+}
+
+long long C(long long n, long long r) {
+    if (n < 0 || r < 0 || r > n) return 0;
+    return fact[n] * invFact[r] % MOD * invFact[n - r] % MOD;
+}
 
 int main() {
-    priority_queue<long long, vector<long long>, greater<long long>> S1;
-    priority_queue<long long> S2;
-    long long X;
-    cin >> X;
-    S2.push(X);
+    long long X_1, X_2, X_3;
+    cin >> X_1 >> X_2 >> X_3;
 
-    int Q;
-    cin >> Q;
-    while(Q--) {
-        long long A, B;
-        cin >> A >> B;
-        long long minS2;
-        minS2 = S2.top();
-
-        if (A <= minS2) S2.push(A);
-        else S1.push(A);
-        if (B <= minS2) S2.push(B);
-        else S1.push(B);
-
-        if ((int)S2.size() > (int)S1.size() + 1) {
-            long long tmp = S2.top();
-            S2.pop();
-            S1.push(tmp);
-        }
-        if ((int)S2.size() < (int)S1.size()) {
-            long long tmp = S1.top();
-            S1.pop();
-            S2.push(tmp);
-        }
-        cout << S2.top() << endl;
+    long long N = X_1 + X_2 + X_3 + 5;
+    fact[0] = 1;
+    for (int i = 1; i <= N; i++) {
+        fact[i] = fact[i - 1] * i % MOD;
     }
+    invFact[N] = modpow(fact[N], MOD - 2);
+    for (int i = N; i >= 1; i--) {
+        invFact[i - 1] = invFact[i] * i % MOD;
+    }
+
+    long long ans = 0;
+
+    for (int i = 0; i <= X_1 && i <= X_2 + 1; i++) {
+        long long waysChooseSlots = C(X_2 + 1, i);
+        long long waysSplitOnes = C(X_1 - 1, i - 1);
+        long long waysPlaceThrees = C(X_2 + X_3 - i, X_3);
+
+        ans += waysChooseSlots * waysSplitOnes % MOD * waysPlaceThrees % MOD;
+        ans %= MOD;
+    }
+
+    cout << ans << endl;
+
     return 0;
 }
