d2af8cdd4660d8489d390e5a18ea1eb059d2e533
Yorihide-Saito <yorihide.saito@gmail.com>
2026-07-05 03:01:11 +0900

    ABC 167 C
    bit 全探索
    - 計算量は、2 ^ N * N * M
       - なので、ワースとケースで、59 万程度
       - ２秒は, 2*10^7 ~ 2*10^8 あたりまで計算可能であるため、余裕
    - INT32_MAX 使用している2 ^ 32 - 1 の値。
       - 1e9 とかでもよかった

--- src/training/01_search/abc167_c.cpp
+++ src/training/01_search/abc167_c.cpp
@@ -3,8 +3,8 @@
 // パターン: bit全探索
 //
 // ---- 3行設計(書いてから実装する) ----
-// ① データ構造:
-// ② ループ/遷移:
+// ① データ構造: vector<int>
+// ② ループ/遷移: for (int i = 0; i < (1 << n); i++)
 // ③ 答え:
 
 #include <bits/stdc++.h>
@@ -15,5 +15,38 @@ int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
+    int n, m, x;
+    cin >> n >> m >> x;
+    vector<int> c(n);
+    vector<vector<int>> a(n, vector<int>(m, 0));
+    for (int i = 0; i < n; i++) {
+        cin >> c[i];
+        for (int j = 0; j < m;j ++) {
+            cin >> a[i][j];
+        }
+    }
+
+    int ans = INT32_MAX;
+    for (int i = 0; i < (1 << n); i++) {
+        int tmp = 0;
+        vector<int> unds(m, 0);
+        for (int j = 0; j < n; j++) {
+            if (i >> j & 1) {
+                tmp += c[j];
+                for (int k = 0; k < m; k++) {
+                    unds[k] += a[j][k];
+                }
+            }
+        }
+        bool isOk = true;
+        for (int j = 0; j < m; j++) {
+            if(unds[j] < x) {
+                isOk  = false;
+            }
+        }
+        if(isOk) ans = min(ans, tmp);
+    }
+
+    cout << (ans == INT32_MAX ? -1 : ans) << endl;
     return 0;
 }
