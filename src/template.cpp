4c0d0b73893946f923e2b4a18b5c21de91436203
Yorihide-Saito <yorihide.saito@gmail.com>
2026-07-05 01:06:43 +0900

    テンプレートファイルの更新

--- src/template.cpp
+++ src/template.cpp
@@ -1,7 +1,33 @@
-#include<iostream>
+#include <bits/stdc++.h>
 using namespace std;
 using ll = long long;
 
+#ifdef LOCAL
+template <class T, class U>
+ostream& operator<<(ostream& os, const pair<T, U>& p) {
+    return os << '(' << p.first << ',' << p.second << ')';
+}
+template <class T>
+ostream& operator<<(ostream& os, const vector<T>& v) {
+    os << '[';
+    for (size_t i = 0; i < v.size(); i++) os << (i ? " " : "") << v[i];
+    return os << ']';
+}
+template <class T> void dbg_out(const T& a) { cerr << a << endl; }
+template <class T, class... U>
+void dbg_out(const T& a, const U&... b) { cerr << a << ", "; dbg_out(b...); }
+#define dbg(...) cerr << "[" << #__VA_ARGS__ << "] = ", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+
 int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    // ① データ構造:
+    // ② ループ/遷移:
+    // ③ 答え:
+
     return 0;
 }
