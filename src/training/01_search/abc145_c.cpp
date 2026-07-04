16a0ed626a8cb3d55e75984267ae111486a89e07
Yorihide-Saito <yorihide.saito@gmail.com>
2026-07-05 01:06:50 +0900

    ABC 145 C 実施
    パターンにおける全探索
    - N < 8 のため全探索で全ての経路を探索する。
    - 組み合わせに関しては、next_permutation(permutation = 順列(組み合わせとかのパーミュテーション))を使用する
       - do - while にする理由は、性質上（1、2、3）の最初のパターン後からの呼び出しとなってしまうから。
       - 終了時には、false が変える
    - factorial(階乗)
    - "fixed << setprecision(x)" で x 桁数分を出力する。
       - セットプレシジョン

--- src/training/01_search/abc145_c.cpp
+++ src/training/01_search/abc145_c.cpp
@@ -15,5 +15,32 @@ int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
+    int n;
+    cin >> n;
+    vector<double> x(n, 0), y(n, 0);
+    for (int i = 0; i < n; i++) {
+        cin >> x[i] >> y[i];
+    }
+
+    auto dist = [&](int i, int j) -> double {
+        double dx = x[j] - x[i];
+        double dy = y[j] - y[i];
+        return sqrt(dx * dx + dy * dy);
+    };
+
+    double ans = 0;
+
+    vector<int> v(n, 0);
+    for (int i = 0; i < n; i++) v[i] = i;
+    do {
+        for(int i = 0; i < n-1; i++) {
+            ans += dist(v[i], v[i+1]);
+        }
+    } while(next_permutation(v.begin(), v.end()));
+    int factorial = 1;
+    for (int i = 2; i <= n; i++) {
+        factorial *= i;
+    }
+    cout << fixed << setprecision(10) << ans / factorial << endl;
     return 0;
 }
