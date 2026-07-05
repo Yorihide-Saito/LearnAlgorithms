dd0bf4d41562db5be170973f5996673b1ec6fa12
Yorihide-Saito <yorihide.saito@gmail.com>
2026-07-05 21:31:48 +0900

    ABC077 C （一回間違えた（N^2 * log N の計算量で TLE））
    lower_bound と　upper_bound を使用した二分探索
    - b を固定して、b より小さい a の数を取得、b より大きい c の数を取得
    - それらの組み合わせを加算していくことにより、以下の計算量を実現している。
        - N * log N

--- src/training/01_search/abc077_c.cpp
+++ src/training/01_search/abc077_c.cpp
@@ -14,6 +14,25 @@ using ll = long long;
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
+    int n;
+    cin >> n;
+    vector<int> a(n), b(n), c(n);
+    for (auto &x : a) cin >> x;
+    for (auto &x : b) cin >> x;
+    for (auto &x : c) cin >> x;
 
+    sort(a.begin(), a.end());
+    sort(b.begin(), b.end());
+    sort(c.begin(), c.end());
+
+    long long ans = 0;
+    for (int i = 0; i < n; i++) {
+        long long acnt = lower_bound(a.begin(), a.end(), b[i]) - a.begin();
+        long long ccnt = c.end() - upper_bound(c.begin(), c.end(), b[i]);
+
+        ans += acnt * ccnt;
+    }
+
+    cout << ans << endl;
     return 0;
 }
