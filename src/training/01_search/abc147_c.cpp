b6fedc4bc171c5ad9537194598f05452d4146a11
Yorihide-Saito <yorihide.saito@gmail.com>
2026-07-05 19:51:09 +0900

    ABC 147 C
    bit 全探索
    - 制約 N < 15 であるため、bit 全探索を考えることができる。
       - 2^N * N * A が計算量であり、ワースケースが 2^15 * 15 * (15 - 1) となるため、実行時間では間に合う。
    - x, y を pair で管理する方が綺麗になったり、for ループないを一時的に変数に置いた方がよかった感がある。

--- src/training/01_search/abc147_c.cpp
+++ src/training/01_search/abc147_c.cpp
@@ -15,5 +15,41 @@ int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
+    int n;
+    cin >> n;
+    vector<vector<int>> x(n, vector<int>()), y(n, vector<int>());
+    vector<int> a(n, 0);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+        for (int j = 0; j < a[i]; j++) {
+            int x_in, y_in;
+            cin >> x_in >> y_in;
+            x_in--;
+            x[i].push_back(x_in);
+            y[i].push_back(y_in);
+        }
+    }
+
+    // 正直者のパターンを考える
+    int ans = 0;
+    for (int bit = 0; bit < (1 << n); bit++) {
+        int isOk = true;
+        int cnt = 0;
+        for (int i = 0; i < n; i++) {
+            if (bit >> i & 1) {
+                for (int j = 0; j < a[i]; j++) {
+                    if ((bit >> x[i][j] & 1) !=  y[i][j]) {
+                        isOk = false;
+                        break;
+                    }
+                }
+                cnt++;
+            }
+        }
+        if(isOk) {
+            ans = max(ans, cnt);
+        }
+    }
+    cout << ans << endl;
     return 0;
 }
