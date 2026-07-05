2caad490f9039d565315bbc3b5363c888464566c
Yorihide-Saito <yorihide.saito@gmail.com>
2026-07-06 01:31:14 +0900

    ABC023 D
    二分探索で可能な範囲を確認する
    - 普通にむずすぎ
    - つまずいたところ
       - ok の数を小さくしすぎて、オーバーフローした
       - 次は大きくしすぎて、途中の計算がオーバーフローした
       - 各風船が特定の時間内に割れるかどうかを計算する部分で mid < h[i] の条件がなくて WA 出まくった
           - 早期リターンが必要な理由は、そもそも mid が h[i] よりも小さということはないから負の値での計算が発生してしまう。
           - そうなると、それも可能である判定を行うようになったいたため、正常な閾値ではないものまで対象となっていた。
    - 他に関しては意外と問題はなかった。
    - ok と ng を逆にした例だったが、正直どっちでもよかったので問題なし。

--- src/training/01_search/abc023_d.cpp
+++ src/training/01_search/abc023_d.cpp
@@ -14,6 +14,36 @@ using ll = long long;
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
+    long long n;
+    cin >> n;
+    vector<long long> h(n), s(n);
+    for (int i = 0; i < n; i++) {
+        cin >> h[i] >> s[i];
+    }
 
+    long long ok = 1LL << 60, ng = -1;
+    auto check = [&] (long long mid) {
+        vector<long long > time(n, 0);
+        for (int i = 0; i < n; i++) {
+            if (mid < h[i]) return false;
+
+            long long diff = mid - h[i];
+            long long sec = diff / s[i];
+            time[i] = sec;
+        }
+        sort(time.begin(), time.end());
+        for (int i = 0; i < n; i++) {
+            if (time[i] < (long long)i) {
+                return false;
+            }
+        }
+        return true;
+    };
+    while(ok - ng > 1) {
+        long long mid = (ng + ok) / 2;
+        (check(mid) ? ok : ng) = mid;
+    }
+
+    cout << ok << endl;
     return 0;
 }
