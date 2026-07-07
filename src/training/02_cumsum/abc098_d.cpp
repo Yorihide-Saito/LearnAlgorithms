389dcaf6d6549b4b984ceed775d8db3c1ae62b40
Yorihide-Saito <yorihide.saito@gmail.com>
2026-07-08 03:23:43 +0900

    ABC098 D
    しゃくとり方
    - 今回の場合は、xor であるため単調性が保証される
       - bit 数が固定であり、Al + ... + Ar が増加するため、xor している場合は単調増加
    - これ難しいな…。（しゃくとり自体が少し苦手）

--- src/training/02_cumsum/abc098_d.cpp
+++ src/training/02_cumsum/abc098_d.cpp
@@ -1,5 +1,5 @@
 // [02-4] ABC098D Xor Sum 2
-// https://atcoder.jp/contests/abc098/tasks/abc098_d
+// https://atcoder.jp/contests/abc098/tasks/arc098_d
 // パターン: 尺取り法+XOR
 //
 // ---- 3行設計(書いてから実装する) ----
@@ -14,6 +14,27 @@ using ll = long long;
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+    vector<long long> a(n, 0);
+    for(auto &x : a) cin >> x;
+
+    long long ans = 0, sum = 0, xr = 0;
+    int r = 0;
+    for (int l = 0; l < n; l++) {
+        while(r < n && sum + a[r] == (xr ^ a[r])) {
+            sum += a[r];
+            xr ^= a[r];
+            r++;
+        }
+
+        ans += r - l;
+        sum -= a[l];
+        xr ^= a[l];
+    }
+
+    cout << ans << endl;
 
     return 0;
 }
