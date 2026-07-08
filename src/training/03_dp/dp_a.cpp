2869cde035187d13dd44aa71a0e5643d0f034092
Yorihide-Saito <yorihide.saito@gmail.com>
2026-07-09 04:44:22 +0900

    DP A
    1 次元の DP
    - prev1, prev2 などの変数を置き換えていく方法でも可能

--- src/training/03_dp/dp_a.cpp
+++ src/training/03_dp/dp_a.cpp
@@ -15,5 +15,19 @@ int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
+    int n;
+    cin >> n;
+    vector<int> h(n);
+    for (auto &x : h) cin >> x;
+
+    vector<int> dp(n + 1, 0);
+    dp[1] = abs(h[0] - h[1]);
+    for(int i = 2; i < n; i++) {
+        dp[i] = min(
+            dp[i - 1] + abs(h[i] - h[i - 1]),
+            dp[i - 2] + abs(h[i] - h[i - 2])
+        );
+    }
+    cout << dp[n - 1] << endl;
     return 0;
 }
