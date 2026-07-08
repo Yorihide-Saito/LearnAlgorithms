55d136f6ffd0b7cb3834aa01e740e18e111e88ca
Yorihide-Saito <yorihide.saito@gmail.com>
2026-07-09 04:17:43 +0900

    Typical90 028
    2次元imos
    - 今回は半区間のため、r をそのまま終端とできる
        - もし閉区間の場合、+1 の座標とする必要がある。
           '''
           grid[lx][ly]
           grid[rx + 1][ly]
           grid[lx][ry + 1]
           grid[rx + 1][ry + 1]
           '''
        - ↑になるはず。
    - 計算量は 約10^6 となるため、実行時間的にも問題はない。

--- src/training/02_cumsum/typical90_028.cpp
+++ src/training/02_cumsum/typical90_028.cpp
@@ -14,6 +14,41 @@ using ll = long long;
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
+    int n;
+    cin >> n;
+    vector<vector<int>> grid(1002, vector<int>(1002, 0));
+    for (int i = 0; i < n; i++) {
+        int lx, ly, rx, ry;
+        cin >> lx >> ly >> rx >> ry;
+        grid[lx][ly] += 1;
+        grid[lx][ry] -= 1;
+        grid[rx][ly] -= 1;
+        grid[rx][ry] += 1;
+
+    }
+
+    for (int i = 0; i <= 1000; i++) {
+        for (int j = 0; j < 1000; j++) {
+            grid[i][j + 1] += grid[i][j];
+        }
+    }
+
+    for (int i = 0; i <= 1000; i++) {
+        for (int j = 0; j < 1000; j++) {
+            grid[j + 1][i] += grid[j][i];
+        }
+    }
+
+    vector<int> ans(n + 1, 0);
+    for (int i = 0; i < 1000; i++) {
+        for (int j = 0; j < 1000; j++) {
+            ans[grid[i][j]]++;
+        }
+    }
+
+    for (int i = 1; i <= n; i++) {
+        cout << ans[i] << endl;
+    }
 
     return 0;
 }
