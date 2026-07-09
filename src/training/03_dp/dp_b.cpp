e30faf21e5fa2d7cfa2688d88c64acfc5d9f1c87
Yorihide-Saito <yorihide.saito@gmail.com>
2026-07-10 01:37:15 +0900

    DP B
    DP
    - DP_A と同じ感じ
    - k 回戻るので現在の i が 0 を下回らないように条件を追加する

--- src/training/03_dp/dp_b.cpp
+++ src/training/03_dp/dp_b.cpp
@@ -15,5 +15,24 @@ int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
+    int n, k;
+    cin >> n >> k;
+    vector<int> h(n, 0);
+    for (auto &x : h) cin >> x;
+
+    const int INF = 1e9;
+    vector<int> dp(n + k, INF);
+
+    dp[0] = 0;
+
+    for (int i = 1; i < n; i++) {
+        for (int j = 1; j <= k; j++) {
+            if (i - j < 0) break;
+            dp[i] = min(dp[i], dp[i - j] + abs(h[i] - h[i - j]));
+        }
+    }
+
+    cout << dp[n - 1] << endl;
+
     return 0;
 }
