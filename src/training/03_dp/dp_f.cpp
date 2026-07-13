f831c595f31c5f86153922a4c3f6a807b2c03bc8
Yorihide-Saito <yorihide.saito@gmail.com>
2026-07-14 00:02:40 +0900

    DP F
    dp
    - 最大文字数 + 1 で dp する。
        - 今回の場合は、横が s 縦が t となる。
    - 例えば、dp[2][3] の場合は、s の2文字目と t の3文字目までの一致数を持つ
    - また最大部分列がわかるため、答えは復元する必要がある。

--- src/training/03_dp/dp_f.cpp
+++ src/training/03_dp/dp_f.cpp
@@ -15,5 +15,42 @@ int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
+    string s, t;
+    cin >> s >> t;
+
+    int ssiz = s.size();
+    int tsiz = t.size();
+
+    vector<vector<int>> dp(3001, vector<int>(3001, 0));
+    for (int i = 1; i <= ssiz; i++) {
+        for (int j = 1; j <= tsiz; j++) {
+            if (s[i - 1] == t[j - 1]) {
+                dp[i][j] = dp[i - 1][j - 1] + 1;
+            }
+            else {
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+    }
+
+    string ans;
+    int i = ssiz, j = tsiz;
+    while(i > 0 && j > 0) {
+        if (s[i - 1] == t[j - 1]) {
+            ans += s[i - 1];
+            i--;
+            j--;
+        }
+        else if(dp[i - 1][j] >= dp[i][j - 1]) {
+            i--;
+        }
+        else {
+            j--;
+        }
+    }
+
+    reverse(ans.begin(), ans.end());
+    cout << ans << endl;
+
     return 0;
 }
