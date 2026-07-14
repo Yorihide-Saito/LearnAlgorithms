9b631670fc092c6b54d6cedacd3fc94da60c4cbf
Yorihide-Saito <yorihide.saito@gmail.com>
2026-07-15 01:06:07 +0900

    DP H
    dp
    - 下、右の単調性を活かして、その時点での最大経路数を保持し続ける
    - 今回のような単調性の場合使用できる

--- src/training/03_dp/dp_h.cpp
+++ src/training/03_dp/dp_h.cpp
@@ -11,9 +11,37 @@
 using namespace std;
 using ll = long long;
 
+const long long MOD = 1000000007;
+
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
+    int h, w;
+    cin >> h >> w;
+    vector<vector<char>> grid(h, vector<char>(w));
+    for (int i = 0; i < h; i++) {
+        for (int j = 0; j < w; j++) {
+            cin >> grid[i][j];
+        }
+    }
+
+    vector<vector<long long>> dp(h, vector<long long>(w, 0));
+    dp[0][0] = 1;
+    for (int i = 0; i < h; i++) {
+        for (int j = 0; j < w; j++) {
+            if (grid[i][j] == '#') continue;
+
+            if (i > 0) {
+                dp[i][j] += dp[i - 1][j];
+            }
+            if (j > 0) {
+                dp[i][j] += dp[i][j - 1];
+            }
+            dp[i][j] %= MOD;
+        }
+    }
+
+    cout << dp[h - 1][w - 1] << endl;
     return 0;
 }
