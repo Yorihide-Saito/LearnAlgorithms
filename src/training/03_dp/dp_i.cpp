950ae335878b01115fe73697aa8b013251391ac9
Yorihide-Saito <yorihide.saito@gmail.com>
2026-07-16 21:24:29 +0900

    DP I
    dp
    - 確率 DP
    - dp 配列では、裏の数を列として持つ
        - その場合 dp[0] は 1.0（すべてが表という判定）
    - 入力で取得したコイン分数を増やしていく
    - 最後に、今回は表の数が多い数であるため n / 2 + 1 以降の dp 配列の合計を取る

--- src/training/03_dp/dp_i.cpp
+++ src/training/03_dp/dp_i.cpp
@@ -15,5 +15,30 @@ int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
+    int n;
+    cin >> n;
+
+    vector<double> dp(n + 1, 0.0);
+    dp[0] = 1.0;
+    for (int i = 0; i < n; i++) {
+        double p;
+        cin >> p;
+
+        vector<double> ndp(n + 1, 0.0);
+        for (int j = 0; j <= i; j++) {
+            ndp[j] += dp[j] * (1.0 - p);
+            ndp[j + 1] += dp[j] * p;
+        }
+
+        dp = move(ndp);
+    }
+
+    double ans = 0.0;
+    for (int i = n / 2 + 1; i <= n; i++) {
+        ans += dp[j];
+    }
+
+    cout << fixed << setprecision(15) << ans << endl;
+
     return 0;
 }
